a418b77af9ecd283ea2d419580a3e660d89cbd38
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-09-16 20:03:04 +0200

    added_docs

--- pusher.sh
+++ pusher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 black "."
 isort .
-pdoc -o docs policeVsProtesters
-mv docs/policeVsProtesters/index.html docs/index.md
-mv docs/policeVsProtesters/* docs/
+pdoc --force --html -o docs pvp
+mv docs/pvp/index.html docs/index.md
+mv docs/pvp/* docs/
 git add . && git commit -m $1 && git push
