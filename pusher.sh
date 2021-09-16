ed82abf2b8055f2f512ee88541bd67a355cfc552
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-09-16 19:50:09 +0200

    better_commit+docs

--- pusher.sh
+++ pusher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 black "."
 isort .
-pdoc --force --html -o docs policeVsProtesters
+pdoc -o docs policeVsProtesters
 mv docs/policeVsProtesters/index.html docs/index.md
 mv docs/policeVsProtesters/* docs/
 git add . && git commit -m $1 && git push
